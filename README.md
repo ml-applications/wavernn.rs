3b165c649d3a46ea89938aa24cdd0ca3ee6c3dd8
Brandon Thomas <echelon@gmail.com>
2019-05-26 01:00:06 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+wavernn.rs
+==========
+WIP implementation of the WaveRNN vocoder in Rust.
---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+// Copyright (c) 2019 Brandon Thomas <bt@brand.io>, <echelon@gmail.com>
+//
+// #![deny(dead_code)]
+// #![deny(missing_docs)]
+// #![deny(unreachable_patterns)]
+// #![deny(unused_extern_crates)]
+// #![deny(unused_imports)]
+// #![deny(unused_qualifications)]
+//
+//! Vocode.rs
+
+#[cfg(test)] #[macro_use] extern crate expectest;
+
+/// Library version string
+pub const VERSION_STRING : &'static str = "0.0.1";
+

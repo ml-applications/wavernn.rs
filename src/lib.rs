4b4ecda18483202c242b38e7f0a3b605dd85a6db
Brandon Thomas <echelon@gmail.com>
2019-05-31 12:47:54 -0400

    Work in progress: matrices, work on library

--- src/lib.rs
+++ src/lib.rs
@@ -19,7 +19,8 @@ extern crate byteorder;
 
 use std::io;
 use std::fs::File;
-use std::io::Cursor;
+use std::io::{Cursor, Read};
+use std::io::Error as IoError;
 
 use byteorder::ReadBytesExt;
 use byteorder::BigEndian;
@@ -31,7 +32,7 @@ use na::zero;
 use na::VecStorage;
 use na::Rotation3;
 use na::Matrix;
-
+use ::LayerType::Conv1d;
 
 /// Library version string
 pub const VERSION_STRING : &'static str = "0.0.1";
@@ -66,10 +67,9 @@ pub fn read_mel_file() -> io::Result<()> {
     *cell = data;
   }
 
-  for (i, mut cell) in mat.iter().enumerate() {
-    println!("i: {}, data: {}", i, cell);
-  }
-
+  // for (i, mut cell) in mat.iter().enumerate() {
+  //   println!("i: {}, data: {}", i, cell);
+  // }
 
   Ok(())
 }
@@ -82,6 +82,14 @@ class Model{
     int total_scale;
     int nPad;
   };
+  Header header;
+
+  UpsampleNetwork upsample;
+  Resnet resnet;
+  TorchLayer I;
+  TorchLayer rnn1;
+  TorchLayer fc1;
+  TorchLayer fc2;
 */
 pub fn read_model_file() -> io::Result<()> {
   type DMatrixf32 = Matrix<f32, Dynamic, Dynamic, VecStorage<f32, Dynamic, Dynamic>>;
@@ -107,9 +115,155 @@ pub fn read_model_file() -> io::Result<()> {
   println!("total_scale: {}", total_scale);
   println!("n_pad: {}", n_pad);
 
+  /*
+  class Resnet {
+    TorchLayer conv_in;
+    TorchLayer batch_norm;
+    ResBlock resblock;
+    TorchLayer conv_out;
+    TorchLayer stretch2d;  //moved stretch2d layer into resnet from upsample as in python code
+  }
+  */
+
+  //TorchLayer::Header header;
+  //fread(&header, sizeof(TorchLayer::Header), 1, fd);
+  /*
+
+    struct  Header{
+        //int size; //size of data blob, not including this header
+        enum class LayerType : int { Conv1d=1, Conv2d=2, BatchNorm1d=3, Linear=4, GRU=5, Stretch2d=6 } layerType;
+        char name[64]; //layer name for debugging
+    };*/
+
+  let layer_type = file.read_i32::<LittleEndian>()?;
+
+  let layer_type = parse_layer_type(layer_type).unwrap();
+
+  println!("layer_type: {:?}", layer_type);
+
+  /*let mut buffer = [0; 64];
+  file.read_exact(&mut buffer);
+  let name = String::from_utf8_lossy(&buffer);*/
+
+  let name = read_name(&mut file).unwrap();
+
+  println!("name: {}", name);
+
+  match (layer_type) {
+    LayerType::Conv1d => {
+      let layer = Conv1dLayer::parse(&mut file)?;
+      println!("Layer: {:?}", layer);
+    },
+    LayerType::Conv2d => {},
+    LayerType::BatchNorm1d => {},
+    LayerType::Linear => {},
+    LayerType::GRU => {},
+    LayerType::Stretch2d => {},
+  }
+
   Ok(())
 }
 
+#[derive(Clone,Copy,Debug)]
+enum LayerType {
+  Conv1d,
+  Conv2d,
+  BatchNorm1d,
+  Linear,
+  GRU,
+  Stretch2d,
+}
+
+// TODO: Use From<> trait.
+fn parse_layer_type(layer_type: i32) -> Option<LayerType> {
+  match layer_type {
+    1 => Some(LayerType::Conv1d),
+    2 => Some(LayerType::Conv2d),
+    3 => Some(LayerType::BatchNorm1d),
+    4 => Some(LayerType::Linear),
+    5 => Some(LayerType::GRU),
+    6 => Some(LayerType::Stretch2d),
+    _ => None,
+  }
+}
+
+fn read_name(file: &mut File) -> Option<String> {
+  let mut buffer = [0; 64];
+  //let mut buffer = String::with_capacity(64usize);
+  file.read_exact(&mut buffer);
+
+  let name = String::from_utf8_lossy(&buffer);
+
+  // TODO: Remove trailing null bytes.
+
+  Some(name.into())
+}
+
+/*
+    struct  Header{
+        int elSize;  //size of each entry in bytes: 4 for float, 2 for fp16.
+        int useBias;
+        int inChannels;
+        int outChannels;
+        int kernelSize;
+    };
+
+    std::vector<Matrixf> weight;
+    Vectorf bias;
+
+    bool hasBias;
+    int inChannels;
+    int outChannels;
+    int nKernel;
+*/
+
+#[derive(Debug)]
+struct Conv1dLayer {
+  el_size: i32,
+  use_bias: bool,
+  in_channels: i32,
+  out_channels: i32,
+  kernel_size: i32,
+}
+
+impl Conv1dLayer {
+  fn parse(file: &mut File) -> io::Result<Conv1dLayer> {
+    let el_size = file.read_i32::<LittleEndian>()?;
+    let use_bias = file.read_i32::<LittleEndian>()?;
+    let in_channels = file.read_i32::<LittleEndian>()?;
+    let out_channels = file.read_i32::<LittleEndian>()?;
+    let kernel_size = file.read_i32::<LittleEndian>()?;
+
+    if el_size != 2 && el_size != 4 {
+      return Err(IoError::from_raw_os_error(0)); // TODO: Actual error
+    }
+
+    let use_bias = match use_bias {
+      0 => false,
+      _ => true,
+    };
+
+    if kernel_size == 1 {
+      // If kernel is 1x then convolution is just matrix multiplication.
+      // Load weight into the first element and handle separately.
+
+    } else {
+
+    }
+
+    if has_bias {
+
+    }
+
+    Ok(Conv1dLayer {
+      el_size,
+      use_bias,
+      in_channels,
+      out_channels,
+      kernel_size,
+    })
+  }
+}
 
 #[cfg(test)]
 mod tests {
